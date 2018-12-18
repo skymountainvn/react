596ae136665654d5210f0caae263101ad62bc7eb
Hung Le <skvn.clew@gmail.com>
2018-12-19 01:55:19 +0700

    if

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const word1= {en:"One",vn:"Mot"}
-    const word2= {en:"Two" ,vn:"Hai"}
-    const word3= {en:"Three" ,vn:"Ba"}
+    const word1= {en:"One",vn:"Mot", isMemorized: true}
+    const word2= {en:"Two" ,vn:"Hai",isMemorized: false}
+    const word3= {en:"Three" ,vn:"Ba",isMemorized: true}
     return (
       <div className="App">
         <Word wordInfo = {word1}/>
--- src/Components/Shared/Word.js
+++ src/Components/Shared/Word.js
@@ -1,12 +1,22 @@
 import React, { Component} from 'react';
 
 export class Word extends Component {
+    getButton() {
+        if (this.props.wordInfo.isMemorized) {
+            return <button className= "btn btn-warning"> FORGOT </button>;
+        }
+        return <button className= "btn btn-success"> MEMORIZED </button>
+    }
     render() {
-        const {en, vn} = this.props.wordInfo;
+        const {en, vn, isMemorized} = this.props.wordInfo;
+        const engClassName = isMemorized ? 'text-success': 'text-danger'
         return (
             <div> 
-            <h3> {en} </h3>
+            <h3 className={engClassName}> 
+                {isMemorized ? '-----': en} 
+            </h3>
             <p> {vn} </p>
+            {this.getButton()}
             </div>
         );
     }
